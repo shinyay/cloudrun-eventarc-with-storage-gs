bbb44b69850219f8ed220fc68553c965f4c1ebe5
shinyay <shinya.com@gmail.com>
2020-11-04 17:40:57 +0900

    Add receive message and return entity function

--- eventarc-storage-getting-started/src/main/kotlin/com/google/shinyay/controller/StorageEventController.kt
+++ eventarc-storage-getting-started/src/main/kotlin/com/google/shinyay/controller/StorageEventController.kt
@@ -1,18 +1,29 @@
 package com.google.shinyay.controller
 
+import com.google.shinyay.logger
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RestController
 
-val cloudEventHeaders = listOf("ce-id", "ce-source", "ce-type", "ce-specversion")
+val cloudEventHeaders = listOf("ce-id", "ce-source", "ce-specversion", "ce-type", "ce-subject")
 
 @RestController
 class StorageEventController {
 
     @PostMapping("/")
     fun receiveMessage(@RequestBody body: Map<String, Any>,
-                       @RequestHeader header: Map<String, String>) {
-
+                       @RequestHeader header: Map<String, String>): ResponseEntity<String> {
+        cloudEventHeaders.forEach {
+            if(header[it] == null) {
+                val message = "Missing expected header: $it."
+                logger.info(message)
+                return ResponseEntity<String>(message, HttpStatus.BAD_REQUEST)
+            }
+        }
+        val message = "Detected change in GCS bucket: ${header["ce-subject"]}"
+        return ResponseEntity<String>(message, HttpStatus.OK)
     }
 }
