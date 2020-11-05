468a7f37769a1bd966d6d39296604212ab7353f0
shinyay <shinya.com@gmail.com>
2020-11-05 13:16:46 +0900

    Add deal with directory path

--- script/build.fish
+++ script/build.fish
@@ -1,6 +1,9 @@
 #!/usr/bin/env fish
 
 function do_func
+  set -l _project cloudrun-eventarc-with-storage-gs
+  set -l _source eventarc-storage-getting-started
+  
 
   argparse -n do_func 'h/help' 'p/project=' -- $argv
   or return 1
@@ -13,7 +16,7 @@ function do_func
   set -lq _flag_project
   or set -l _flag_project (gcloud config get-value project)
 
-  gcloud alpha builds submit --pack image=gcr.io/$_flag_project/storage-event-receiver
+  gcloud alpha builds submit (pwd |awk -F "/$_project" '{print $1}')/$_project/$_source --pack image=gcr.io/$_flag_project/storage-event-receiver
 
 end
 
