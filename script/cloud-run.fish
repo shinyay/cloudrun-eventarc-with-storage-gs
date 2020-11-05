78fd0f98b6355bf3c4994d0e42a01756595d20c2
shinyay <shinya.com@gmail.com>
2020-11-05 13:58:07 +0900

    Modify app name as variable

--- script/cloud-run.fish
+++ script/cloud-run.fish
@@ -5,21 +5,19 @@ function do_func
   set -l _source eventarc-storage-getting-started
   set -l _app storage-event-receiver
   
-
   argparse -n do_func 'h/help' 'p/project=' -- $argv
   or return 1
 
   if set -lq _flag_help
-    echo "build.fish -p/--project <PROJECT_ID>"
+    echo "cloud-run.fish -p/--project <PROJECT_ID>"
     return
   end
 
   set -lq _flag_project
   or set -l _flag_project (gcloud config get-value project)
 
-  gcloud alpha builds submit (pwd |awk -F "/$_project" '{print $1}')/$_project/$_source --pack image=gcr.io/$_flag_project/$_app
-  gcloud run deploy helloworld-events \
-    --image gcr.io/$(gcloud config get-value project)/helloworld-events \
+  gcloud run deploy $_app \
+    --image gcr.io/$_flag_project/$_app \
     --allow-unauthenticated
 
 end
